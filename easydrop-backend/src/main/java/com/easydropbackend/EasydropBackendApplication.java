974578e221e8cabf43b2f1320104449d3d40fae6
Alexandru Nica <anica@192-168-0-173.rdsnet.ro>
2022-12-25 20:34:48 +0200

    Added Backend Scheleton

---
+++ easydrop-backend/src/main/java/com/easydropbackend/EasydropBackendApplication.java
@@ -0,0 +1,13 @@
+package com.easydropbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasydropBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasydropBackendApplication.class, args);
+	}
+
+}
---
+++ easydrop-backend/src/test/java/com/easydropbackend/EasydropBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easydropbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasydropBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
