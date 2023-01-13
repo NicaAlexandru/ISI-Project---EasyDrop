9fe2a610789a5c28f0973f55a4bb2d70f798dc76
unknown <nica.alexandruclaudiu15@gmail.com>
2023-01-13 22:53:57 +0200

    added forgot-password

--- easydrop-front/src/app/pages/Forms/app.forgotPassword.ts
+++ easydrop-front/src/app/pages/Forms/app.forgotPassword.ts
@@ -1,4 +1,9 @@
 import {Component} from "@angular/core";
+import {AppUser} from "../../models/appUser";
+import {HttpErrorResponse} from "@angular/common/http";
+import {AppUserService} from "../../services/appUser.service";
+import {Router} from "@angular/router";
+import {DataService} from "../../services/data.service";
 
 @Component({
   selector: 'app-forgotPassword',
@@ -6,5 +11,20 @@ import {Component} from "@angular/core";
   styleUrls: ['./app.forgotPassword.css']
 })
 export class AppForgotPassword {
+  private appUser!: AppUser;
 
+  constructor(private appUserService: AppUserService, private dataService: DataService, private router: Router) { }
+
+  onSubmit(userItem: AppUser) {
+      this.appUserService.getAppUserByEmail(userItem.email).subscribe(
+        (response: AppUser) => {
+          this.appUser = response;
+          this.dataService.setForgotUserDetails(this.appUser);
+          this.router.navigate(['/get-password'])
+        },
+        (error: HttpErrorResponse) => {
+          alert("Unknown email!");
+        }
+      );
+  }
 }
